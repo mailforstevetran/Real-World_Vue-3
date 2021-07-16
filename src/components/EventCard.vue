41c443da36c95bc578f2365ddf09346e46ccb17f
Steve Tran <mailforstevetran@gmail.com>
2021-07-16 13:38:57 +0900

    a

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="event-card">
-    
+    <!-- Display event data -->
   </div>
 </template>
 
@@ -10,10 +10,26 @@ export default {
   // props: {
   //   msg: String
   // }
+  data() {
+    return {
+      
+    }
+  }
 };
 </script>
 
 
 <style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
 
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
 </style>
