90b3c22d7c3bf1a932ff064d2b40f8b03eac92bb
Steve Tran <mailforstevetran@gmail.com>
2021-07-16 13:36:54 +0900

    working

---
+++ src/components/EventCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="event-card">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  // props: {
+  //   msg: String
+  // }
+};
+</script>
+
+
+<style scoped>
+
+</style>
